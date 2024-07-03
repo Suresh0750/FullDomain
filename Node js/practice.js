65b783e0a79b446297313cfc01072a3b603606f0
Suresh0750 <sureshvarma0750@gmail.com>
2024-07-02 22:51:39 -0700

    start node js

---
+++ Node js/practice.js
@@ -0,0 +1,20 @@
+
+
+// # http request using node js
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    const url = req.url
+    const method = req.method
+
+    console.log(url,method)
+
+    res.writeHead(200,{'contentType':'text/plain'})
+    res.write('hellow')
+    res.write('riyas')
+    res.end()
+})
+
+server.listen(3000,()=>{
+    console.log(`http://localhost:3000`)
+})
