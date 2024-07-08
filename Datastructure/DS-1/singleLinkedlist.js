63a665e8e68a7b685eb4e39b33f6653ad4f54c3b
Suresh0750 <sureshvarma0750@gmail.com>
2024-07-08 10:02:00 -0700

    data structure sinlge linkedlist

---
+++ Datastructure/DS-1/singleLinkedlist.js
@@ -0,0 +1,171 @@
+
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+    }
+}
+
+class singleLinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.size = 0
+    }
+    append(val){
+        const node = new Node(val)
+        
+        if(!this.head){
+            this.head = node
+            this.tail = node
+        }else{
+            this.tail.next = node
+            this.tail = node
+        }
+        this.size++
+    }
+    prepand(val){
+        const node = new Node(val)
+        if(!this.head){
+            this.head = node
+            this.tail = node
+        }else{
+            node.next = this.head
+            this.head = node
+        }
+        this.size++
+    }
+    pop(){
+        
+        if(!this.head) return undefined
+        
+         let val
+        if(this.size==1){
+             val = this.head
+             this.head = null
+             this.tail = null
+        }else{
+            val = this.tail
+            let curr = this.head
+            let i = 0
+            console.log(`size`,this.size)
+            while(i<this.size-1){
+                curr=curr.next
+                i++
+            }
+            curr.next = null
+            console.log(curr)
+            this.tail = curr
+        }
+        this.size--
+        return val
+    }
+    shift(){
+        if(!this.head) return undefined
+        
+        let curr 
+        if(this.size==1){
+            curr = this.head
+            this.head = null
+            this.tail = null
+        }else{
+            curr = this.head
+            this.head = this.head.next
+        }
+        this.size--
+        return curr
+    }
+    insert(index,val){
+        if(index<0 || this.size<index) return false
+        
+        const node = new Node(val)
+        
+        if(index==0){
+            node.next = this.head
+            this.head  = node
+        }else{
+            let curr = this.head
+            for(let i=0;i<index-1;i++){
+                curr = curr.next
+            }
+            node.next = curr.next
+            curr.next = node
+        }
+        this.size++
+        return true
+    }
+    reverse(){
+        if(!this.size) return undefined
+        
+        if(this.size==1) return this.head
+        
+        let curr = this.head
+        this.tail = this.head
+        let prev = null
+        while(curr){
+            let temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        }
+        
+        this.head = prev
+        // console.log(this.head)
+        return this.head
+    }
+    removeFromval(val){
+        if(!this.size) return undefined
+        
+        if(this.head.val==val){
+            this.head = this.head.next
+            if(this.size==1){
+                this.head = null
+                this.tail = null
+            }
+            this.size--
+            return head
+        }else if(this.tail.val == val){
+            let curr = this.head
+            while(curr.next.val!==val){
+                curr = curr.next
+            }
+            curr.next  = null
+            this.tail = curr
+            this.size--
+            return this.head
+        }else{
+            let curr = this.head
+            while(curr.val !== val){
+                curr = curr.next
+            }
+            curr.val = curr?.next?.val
+            curr.next = curr?.next?.next
+        this.size--
+        return this.head
+        }
+        
+        return 'value is not here'
+    }
+}
+
+const sl = new singleLinkedList()
+sl.append(3)
+sl.append(4)
+sl.append(5)
+sl.append(6)
+sl.append(7)
+sl.prepand(8)
+sl.prepand(18)
+// console.log(sl)
+// console.log(sl.pop())
+// console.log(sl)
+// console.log(sl.shift())
+// console.log(sl)
+sl.insert(0,9)
+sl.insert(1,19)
+console.log(sl)
+// sl.removeFromval(19)
+sl.removeFromval(7)
+console.log(JSON.stringify(sl))
+// sl.reverse()
--- Javascript/Review.js
+++ Javascript/Review.js
@@ -1,109 +1,136 @@
 
 
-// // // // // let x = 5
+// // // // // // let x = 5
 
-// // // // // function pure(...arg){
+// // // // // // function pure(...arg){
     
-// // // // //     console.log(arg)
+// // // // // //     console.log(arg)
     
-// // // // //    let sum = arg.reduce((acc,cur)=>{
-// // // // //        return  acc+=cur
-// // // // //     })
-// // // // //     console.log(sum)
-// // // // //     return sum
-// // // // // }
+// // // // // //    let sum = arg.reduce((acc,cur)=>{
+// // // // // //        return  acc+=cur
+// // // // // //     })
+// // // // // //     console.log(sum)
+// // // // // //     return sum
+// // // // // // }
 
 
-// // // // // pure(1,2,3,4,5,5,6)
-// // // // const arr1 = [1,5,3,2]
-// // // // const arr2 = [8,2,6,1]
+// // // // // // pure(1,2,3,4,5,5,6)
+// // // // // const arr1 = [1,5,3,2]
+// // // // // const arr2 = [8,2,6,1]
 
-// // // // let newArr = []
+// // // // // let newArr = []
 
 
 
-// // // // const newArr1 = arr1.concat(arr2)
+// // // // // const newArr1 = arr1.concat(arr2)
 
-// // // let obj1 = { key: 1 };
-// // // let obj2 = obj1;
-// // // let obj3 = obj2;
+// // // // let obj1 = { key: 1 };
+// // // // let obj2 = obj1;
+// // // // let obj3 = obj2;
 
-// // // obj1.key = 2;
-// // // obj2 = { key: 3 };
+// // // // obj1.key = 2;
+// // // // obj2 = { key: 3 };
 
-// // // console.log(obj1.key, obj2.key, obj3.key)
+// // // // console.log(obj1.key, obj2.key, obj3.key)
 
-// async function asyncFunc() {
-//     console.log('A');
-//     await new Promise(resolve => {
-//      console.log('E')
-//      setTimeout(resolve, 1000)
-//     });
-//     console.log('B');
-//  }
-//  console.log('C');
-//  asyncFunc();
-//  console.log('D');
+// // async function asyncFunc() {
+// //     console.log('A');
+// //     await new Promise(resolve => {
+// //      console.log('E')
+// //      setTimeout(resolve, 1000)
+// //     });
+// //     console.log('B');
+// //  }
+// //  console.log('C');
+// //  asyncFunc();
+// //  console.log('D');
 
-// //  // CDAEB
-// //  // CDABE
-// const obj = {
-//     a: "foo",
-//     b: function () {
-//       console.log(this.a);
-//     },
-//   };
+// // //  // CDAEB
+// // //  // CDABE
+// // const obj = {
+// //     a: "foo",
+// //     b: function () {
+// //       console.log(this.a);
+// //     },
+// //   };
   
-//   const c = obj.b;
+// //   const c = obj.b;
   
-//   obj.b(); // * foo
-//   c();   
-//  // "foo"
+// //   obj.b(); // * foo
+// //   c();   
+// //  // "foo"
 
-// //  c.call(obj)
+// // //  c.call(obj)
 
 
-let jaggedArr = [
-    [1,2,3,4]
-    [1,2,3]
-    [1,2]
-]
+// let jaggedArr = [
+//     [1,2,3,4]
+//     [1,2,3]
+//     [1,2]
+// ]
 
-let makeArr = []
-function recurseHelp(arr){
-    console.log
-    for(let i =0 ;i<arr.length;i++){
+// let makeArr = []
+// function recurseHelp(arr){
+//     console.log
+//     for(let i =0 ;i<arr.length;i++){
 
-        if(Array.isArray(arr[i])){
-            recurseHelp(arr[i])
-        }else{
-            makeArr.push(arr[i])
-        }
-    }
+//         if(Array.isArray(arr[i])){
+//             recurseHelp(arr[i])
+//         }else{
+//             makeArr.push(arr[i])
+//         }
+//     }
+// }
+
+// recurseHelp(jaggedArr)
+
+// console.log(makeArr)
+
+
+
+
+
+// // const http = require('http')
+
+// // const server = http.createServer((req,res)=>{
+
+// //     const url = req.url
+// //     const method = req.method
+
+// //     // res.writeHead(200,{})
+
+// //     res.write(" developer")
+// // })
+
+
+
+// // server.listen(3000,()=>{
+// //     console.log(`http://localhost:3000`)
+// // })
+
+
+// const parent = ()=>{
+//     console.log(this)
+// }
+
+// parent()
+'use strict'
+
+function check(){
+    console.log(this)
 }
+window.check()
 
-recurseHelp(jaggedArr)
+// const obj1 = {
+//     name : 'madhav',
+//     reg : function(){
+//         console.log(this)
+//     },
+//     arr :()=>{
+//         'use strict'
+//         console.log(this)
+//     }
+// }
 
-console.log(makeArr)
-
-
-
-
-
-// const http = require('http')
-
-// const server = http.createServer((req,res)=>{
-
-//     const url = req.url
-//     const method = req.method
-
-//     // res.writeHead(200,{})
-
-//     res.write(" developer")
-// })
-
-
-
-// server.listen(3000,()=>{
-//     console.log(`http://localhost:3000`)
-// })
+// obj1.reg()
+// obj1.arr()
