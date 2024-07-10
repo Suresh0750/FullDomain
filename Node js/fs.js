7e31f97961ce78015ee911decadb2ddf0a089ca3
Suresh0750 <sureshvarma0750@gmail.com>
2024-07-10 10:44:39 -0700

    Node js fs module practice

---
+++ Node js/fs.js
@@ -0,0 +1,40 @@
+
+
+
+const fs = require('fs')
+
+const path = 'info.txt'
+
+fs.writeFile(path,'hello world pranav',(err,data)=>{
+ 
+    if(err){
+        console.log(`err\n ${err}`)
+    }else{
+        console.log(data)
+    }
+})
+
+fs.appendFile(path,'\n hellow developer',(err,data)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
+    }
+})
+
+fs.readFile(path,(err,data)=>{
+    if(err){
+        console.log(`read file ${err}`)
+    }else{
+        console.log(`read file \n ${data}`)
+    }
+})
+
+
+fs.unlink('delete.txt',(err,data)=>{  // evey think is working
+    if(err){
+        console.log(`unlink ${err}`)
+    }else{
+        console.log(`unlink ${data}`)
+    }
+})
